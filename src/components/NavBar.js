dae6353f8d6a4c22a2501a9dcd117dba66e6a075
Jason Zhou <jzhou35@buffalo.edu>
2021-02-01 17:11:22 -0500

    commit tree 240d824bff182ea364572c0d07789ac4ea3dec7d

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,10 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const liLinks = links.map((link,index) => {
+    return <a key={index} href={"#"+link}>{link}</a>
+  })
+  return <nav>{liLinks}</nav>;
 }
 
 export default NavBar;
